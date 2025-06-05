5f2416d494a358e6b60d10f3c44e55099153a90c
p1k0chu <anatoliyid0207@gmail.com>
2025-06-05 23:12:50 +0300

    gradle init time

---
+++ common/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    `java-library`
+}
+
+group = "io.github.leaflowmc"
+version = project.properties["leaflow_version"]!!
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation(libs.junit.jupiter.engine)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
