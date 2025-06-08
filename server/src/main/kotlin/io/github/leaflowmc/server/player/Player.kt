da6576fec1ce81c37debec7e1efb9323dded5c26
p1k0chu <anatoliyid0207@gmail.com>
2025-06-08 14:34:36 +0300

    add packet writing to tcp code

--- server/src/main/kotlin/io/github/leaflowmc/server/player/Player.kt
+++ server/src/main/kotlin/io/github/leaflowmc/server/player/Player.kt
@@ -1,7 +1,13 @@
 package io.github.leaflowmc.server.player
 
+import io.github.leaflowmc.protocol.packets.ClientPacket
 import io.github.leaflowmc.protocol.packets.ProtocolStage
 
 interface Player {
     var protocolStage: ProtocolStage
+
+    /**
+     * Sends the packet to the player.
+     */
+    suspend fun sendPacket(packet: ClientPacket)
 }
--- server/src/main/kotlin/io/github/leaflowmc/server/player/PlayerImpl.kt
+++ server/src/main/kotlin/io/github/leaflowmc/server/player/PlayerImpl.kt
@@ -1,10 +1,23 @@
 package io.github.leaflowmc.server.player
 
+import io.github.leaflowmc.protocol.packets.ClientPacket
 import io.github.leaflowmc.protocol.packets.ProtocolStage
-import io.ktor.network.sockets.Socket
+import io.ktor.network.sockets.*
+import kotlinx.coroutines.channels.Channel
 
 open class PlayerImpl(
-    val socket: Socket
+    private val socket: Socket,
+    private val packetsChannel: Channel<ClientPacket>
 ) : Player {
     override var protocolStage = ProtocolStage.HANDSHAKE
+
+    /**
+     * Sends the packet to the packets channel,
+     * possibly suspending until someone receives it
+     *
+     * @see Player.sendPacket
+     */
+    override suspend fun sendPacket(packet: ClientPacket) {
+        packetsChannel.send(packet)
+    }
 }
