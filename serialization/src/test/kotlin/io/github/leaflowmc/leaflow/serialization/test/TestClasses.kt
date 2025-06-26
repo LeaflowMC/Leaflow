09fe297c859f4ff27424d9ca2459bd2757e2d047
p1k0chu <anatoliyid0207@gmail.com>
2025-06-27 00:57:52 +0300

    add more classes for serialization tests

--- serialization/src/test/kotlin/io/github/leaflowmc/leaflow/serialization/test/TestClasses.kt
+++ serialization/src/test/kotlin/io/github/leaflowmc/leaflow/serialization/test/TestClasses.kt
@@ -1,7 +1,10 @@
 package io.github.leaflowmc.leaflow.serialization.test
 
 import io.github.leaflowmc.leaflow.common.serializer.AnyToNbtSerializer
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonClassDiscriminator
 
 @Serializable
 data class Person(val name: String, val age: Int)
@@ -14,3 +17,24 @@ typealias CarAsNbt = @Serializable(CarAsNbtSerializer::class) Car
 
 @Serializable
 data class Something(val car: CarAsNbt, val person: Person)
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializable
+@JsonClassDiscriminator("glasses_for")
+sealed interface Glasses
+
+@Serializable
+@SerialName("sun")
+data class SunGlasses(
+    @SerialName("tint_level")
+    val tintLevel: Float
+) : Glasses
+
+@Serializable
+@SerialName("vision")
+data class VisionGlasses(
+    @SerialName("vision_left")
+    val visionLeft: Int,
+    @SerialName("vision_right")
+    val visionRight: Int
+) : Glasses
