4ef4a869d3fb07d444064a66a98a97a2d046fe7f
p1k0chu <anatoliyid0207@gmail.com>
2025-06-28 16:38:15 +0300

    add constants

---
+++ server/src/main/kotlin/io/github/leaflowmc/leaflow/server/constants/EncryptionConstants.kt
@@ -0,0 +1,7 @@
+package io.github.leaflowmc.leaflow.server.constants
+
+object EncryptionConstants {
+    const val SERVER_KEY_PAIR_ALGORITHM = "RSA"
+    const val ENCRYPTION_CIPHER = "AES/CFB8/NoPadding"
+    const val ENCRYPTION_ALGORITHM = "AES"
+}
