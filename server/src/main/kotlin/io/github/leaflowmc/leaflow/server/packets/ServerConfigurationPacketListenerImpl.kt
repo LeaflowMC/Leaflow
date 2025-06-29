85373813ef06e9e80f88d83d0fe238f60b8d158a
p1k0chu <anatoliyid0207@gmail.com>
2025-06-29 03:22:40 +0300

    add configuration packet listener impl

---
+++ server/src/main/kotlin/io/github/leaflowmc/leaflow/server/packets/ServerConfigurationPacketListenerImpl.kt
@@ -0,0 +1,43 @@
+package io.github.leaflowmc.leaflow.server.packets
+
+import io.github.leaflowmc.leaflow.protocol.ProtocolStage
+import io.github.leaflowmc.leaflow.protocol.listener.server.ServerConfigurationPacketListener
+import io.github.leaflowmc.leaflow.protocol.packets.common.ClientboundConfigurationKeepAlivePacket
+import io.github.leaflowmc.leaflow.protocol.packets.configuration.ClientboundFinishConfigurationPacket
+import io.github.leaflowmc.leaflow.protocol.packets.configuration.ServerboundAcknowledgeFinishConfigurationPacket
+import io.github.leaflowmc.leaflow.protocol.packets.configuration.ServerboundClientInfoPacket
+import io.github.leaflowmc.leaflow.protocol.packets.configuration.ServerboundKnownPacksPacket
+import io.github.leaflowmc.leaflow.protocol.packets.ping.ClientboundConfigurationPingPacket
+import io.github.leaflowmc.leaflow.protocol.packets.ping.ClientboundPingPacket
+import io.github.leaflowmc.leaflow.server.packets.api.LeaflowServerConfigurationPacketListener
+import io.github.leaflowmc.leaflow.server.player.PlayerConnection
+import kotlinx.coroutines.*
+import org.apache.logging.log4j.LogManager
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.DurationUnit
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class ServerConfigurationPacketListenerImpl(
+    override val playerConnection: PlayerConnection
+) : ServerCommonPacketListenerImpl(), LeaflowServerConfigurationPacketListener {
+    init {
+        playerConnection.sendPacket(
+            ClientboundFinishConfigurationPacket()
+        )
+    }
+
+    override fun clientInfo(packet: ServerboundClientInfoPacket) {
+    }
+
+    override fun knownPacks(packet: ServerboundKnownPacksPacket) {
+    }
+
+    override fun acknowledgeFinishConfiguration(packet: ServerboundAcknowledgeFinishConfigurationPacket) {
+        println("switching to play")
+        playerConnection.setProtocol(ProtocolStage.PLAY)
+    }
+
+    override fun getPingPacket(timestamp: Int): ClientboundPingPacket<*, *> {
+        return ClientboundConfigurationPingPacket(timestamp)
+    }
+}
