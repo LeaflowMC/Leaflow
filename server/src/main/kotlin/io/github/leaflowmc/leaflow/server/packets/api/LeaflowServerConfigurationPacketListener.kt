bd2299e41a72c1b77f0e71e609c937329d571f8b
p1k0chu <anatoliyid0207@gmail.com>
2025-06-29 03:13:01 +0300

    add server specific api interfaces for packet listeners

---
+++ server/src/main/kotlin/io/github/leaflowmc/leaflow/server/packets/api/LeaflowServerConfigurationPacketListener.kt
@@ -0,0 +1,6 @@
+package io.github.leaflowmc.leaflow.server.packets.api
+
+import io.github.leaflowmc.leaflow.protocol.listener.server.ServerConfigurationPacketListener
+
+interface LeaflowServerConfigurationPacketListener : LeaflowServerCommonPacketListener, ServerConfigurationPacketListener {
+}
