e88e54de47ec93437392a494da6861c1f40ed8c3
p1k0chu <anatoliyid0207@gmail.com>
2025-06-30 22:26:53 +0300

    add server brand

--- server/src/main/kotlin/io/github/leaflowmc/leaflow/server/LeaflowServer.kt
+++ server/src/main/kotlin/io/github/leaflowmc/leaflow/server/LeaflowServer.kt
@@ -15,6 +15,15 @@ interface LeaflowServer {
      * the description of the server in server list
      */
     val motd: TextComponent?
+
+    /**
+     * Brand of this server, sent in `ClientboundPluginMessagePacket`
+     * as a `BrandPluginMessage`
+     *
+     * @see io.github.leaflowmc.leaflow.server.packets.plugin_message.BrandPluginMessage
+     */
+    val brand: String
+
     val authEnabled: Boolean
     val keyPair: KeyPair
 
