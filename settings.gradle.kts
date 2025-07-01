befe7f3a09f36b737450e2e5f827957c4edcbb95
p1k0chu <anatoliyid0207@gmail.com>
2025-07-01 18:45:42 +0300

    build: bump kotlin version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
-    kotlin("jvm") version "2.1.20" apply false
+    kotlin("jvm") version "2.2.0" apply false
 }
 
 rootProject.name = "leaflow"
