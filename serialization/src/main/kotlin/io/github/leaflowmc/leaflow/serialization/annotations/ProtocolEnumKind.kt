8fb1e92f6b3c8a68b08ae1d8ce58ebf75138ff73
p1k0chu <anatoliyid0207@gmail.com>
2025-07-01 19:07:33 +0300

    fix(serialization): apply ProtocolEnumKind annotation to properties

--- serialization/src/main/kotlin/io/github/leaflowmc/leaflow/serialization/annotations/ProtocolEnumKind.kt
+++ serialization/src/main/kotlin/io/github/leaflowmc/leaflow/serialization/annotations/ProtocolEnumKind.kt
@@ -1,7 +1,12 @@
 package io.github.leaflowmc.leaflow.serialization.annotations
 
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialInfo
 import kotlinx.serialization.descriptors.SerialDescriptor
 
+@OptIn(ExperimentalSerializationApi::class)
+@SerialInfo
+@Target(AnnotationTarget.PROPERTY)
 annotation class ProtocolEnumKind(val kind: Kind) {
     companion object {
         val DEFAULT = Kind.VAR_INT
